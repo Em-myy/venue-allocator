f89e5f0cd54619df4618a6247e26851b8b0db2b2
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-12-24 12:06:27 +0100

    aaaaaaaaaaa

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
+  xxx;
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
