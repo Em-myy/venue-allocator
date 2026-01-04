35a5c971fe53767bcd5c75be4805e982255114ae
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-01-04 18:02:48 +0100

    aaaa

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,11 @@
+import type { ReactNode } from "react";
+
 interface AuthContextType {
   user: any | null;
   logout: () => void;
   loading: boolean;
+}
+aaaaaaa
+interface AuthProviderType {
+  children: ReactNode;
 }
