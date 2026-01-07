2ff1f7869a3f674f60379e0e98e0bfafd70d5b26
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-01-07 19:29:09 +0100

    working on the web socket

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,37 @@ import userRoutes from "./routes/userAuthentication.js";
 import adminRoutes from "./routes/admin.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
-const server = express();
 
-server.use(express.json());
-server.use(cookieParser());
-server.use(
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  },
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
 
-server.use("/api/authentication", userRoutes);
-server.use("/api/admin", adminRoutes);
+app.use("/api/authentication", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    server.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is listening on port 3000");
     });
   })
